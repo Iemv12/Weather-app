9df5db17761ea9bf374b2d7e17a361948dd3d984
Ivan Matos <ivanmatosvillar@gmail.com>
2021-06-16 00:29:46 -0400

    create  a new scrollbar

--- src/components/section/SectionElements.js
+++ src/components/section/SectionElements.js
@@ -8,6 +8,29 @@ export const SectionContainer = styled.div`
   overflow: scroll;
   overflow-x: hidden;
 
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #f5f5f5;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-image: -webkit-gradient(
+      linear,
+      left bottom,
+      left top,
+      color-stop(0.44, rgb(122, 153, 217)),
+      color-stop(0.72, rgb(73, 125, 189)),
+      color-stop(0.86, rgb(28, 58, 148))
+    );
+  }
+
   @media screen and (min-width: 768px) {
     flex: 1;
     padding: 20px 80px;
